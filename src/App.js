b9fcce2860b3ecf47f9b23e44669473e8e5f28a7
phluidaungyezaw <116444104+AungYeZawDev@users.noreply.github.com>
2024-03-28 19:46:14 +0630

    Memo function

--- src/App.js
+++ src/App.js
@@ -1,34 +1,85 @@
-import React, { useState } from "react";
+import { memo, useState } from 'react';
 
-import "./App.css";
+export default function App() {
 
-import Header from "./Header";
+  const [name, setName] = useState('');
 
-const App = () => {
-
-  console.log("Rendering Form");
-
-  const [name, setName] = useState("");
+  const [address, setAddress] = useState('');
 
   return (
 
-    <div className="App">
+    <>
 
-      <Header title="Input Field" />
+      <label>
 
-      <input
+        Name{': '}
 
-        type="text"
+        <input value={name} onChange={e => setName(e.target.value)} />
 
-        value={name}
+      </label>
 
-        onChange={(e) => setName(e.target.value)}
-      />
+      <label>
 
-    </div>
+        Address{': '}
+
+        <input value={address} onChange={e => setAddress(e.target.value)} />
+
+      </label>
+
+      <Greeting name={name} />
+
+    </>
 
   );
 
-};
+}
 
-export default App;
+const Greeting = memo(function Greeting({ name }) {
+
+  console.log("Greeting was rendered at", new Date().toLocaleTimeString());
+
+  return <h3>Hello{name && ', '} {name}!</h3>;
+
+});
+
+function GreetingSelector({ value, onChange }) {
+
+  return (
+
+    <>
+
+      <label>
+
+        <input
+
+          type="radio"
+
+          checked={value === 'Hello'}
+
+          onChange={e => onChange('Regular')}
+
+        />
+
+        Regular greeting
+
+      </label>
+
+      <label>
+
+        <input
+
+          type="radio"
+
+          checked={value === 'Hello and welcome'}
+
+          onChange={e => onChange('Hello and welcome Enthusiastic')}
+
+          Enthusiastic greeting />
+
+      </label>
+
+    </>
+
+  );
+
+}
